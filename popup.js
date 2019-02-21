3b2606f77e209c38efae3df1ca9d7b4bff4fb4f9
niveinii <44360733+niveinii@users.noreply.github.com>
2019-02-21 10:04:33 +0200

    Add files via upload

--- popup.js
+++ popup.js
@@ -2,8 +2,7 @@ let api = document.getElementById("api")
 function rest(){
 
     fetch('https://sapjira.wdf.sap.corp/rest/api/latest/issue/EUXTEAMNILI-3040').then(response=>(response.json())).then(res=>{
-        api.innerHTML = res
-         console.log(res)})
+        api.innerHTML = res})
 }
 
 rest()
